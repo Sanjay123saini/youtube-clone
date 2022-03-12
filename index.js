3f524b83ec4b14d1cbca72a275592e7a53781760
Sanjay123saini <97749659+Sanjay123saini@users.noreply.github.com>
2022-03-12 18:16:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ let div = document.getElementById("videodiv");
 async function display(){
 
     // q = Popular Videos --> Popular%20Videos (%20 -> Space)
-   // https://www.youtube.com/results?search_query=kids+rhymes
-    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=kids+rhymes
-    &key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=1`);
+
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=popular%20videos&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
 
     let data = await res.json();
     
@@ -33,7 +32,6 @@ async function display(){
     {
     let videodiv= document.createElement("iframe")
     videodiv.setAttribute("class","mons")
-    
     videodiv.src = `https://www.youtube.com/embed/${videoId}`;
     videodiv.allow = 'fullscreen'
     div.append(videodiv)
@@ -54,15 +52,14 @@ async function searchVideos(){
     let query = document.getElementById("video").value; // Search String/ Query String
 
 //search for videos
-let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}
-&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults`);
+let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
 let data = await res.json();
 
 for({id:{videoId}}of data.items)
 {
 let videodiv= document.createElement("iframe")
 videodiv.setAttribute("class","mons")
-videodiv.src = `https://www.youtube.com/embed/${videoId}` ;
+videodiv.src = `https://www.youtube.com/embed/${videoId}`;
 videodiv.allow = 'fullscreen'
 div.append(videodiv)
 }
